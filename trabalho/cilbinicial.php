7350b3a81e586a3833ec6d252e7c3a4dec6e2a78
juliacode2 <2126juba@gmail.com>
2025-05-17 00:14:29 -0300

    LumeDigital
    projeto de conclusão de curso

--- trabalho/cilbinicial.php
+++ trabalho/cilbinicial.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="index.css">
   <link rel="stylesheet" href="menu.css">
-  <link rel="stylesheet" href="../css/barra.css">
+  <link rel="stylesheet" href="barra.css">
   <link rel="php" href="../php/pesquisa.php">
   
   
@@ -16,7 +16,13 @@
 
   <nav class="navbar">
     <div class="logo">Lume Digital</div>
-    <
+    <center>
+
+      <form class="search-box"method="post">
+        <input type="text" name="pesquisa" placeholder="Pesquisar...">
+        <button type="submit"></button>
+    </form> 
+    
 </center>
 
    <li> <h2>BIBLIOTECA</h2></li>
@@ -62,7 +68,7 @@
             <br>
             <li>
                 <i class="fas fa-question-circle"></i>
-                <a href="#">Suporte</a>
+                <a href="sobre.php">Suporte</a>
             </li>
         </ul>
     </nav>
@@ -80,35 +86,73 @@
               <i class="fas fa-bars"></i>
           </label>
      
+</nav>
+
+    <div class="menu">
+      <input type="checkbox" id="check">
+      <div class="menu-icon">
+        <li>
+          <label for="check">
+              <i class="fas fa-bars"></i>
+          </label>
+     
 </div>
 </li>
 
 
-  <div class="si
-<center>
- 
-</body>
-</html>
-debar_menup">
+  <div class="sidebar_menup">
       <div class="sidebar_titulop">
            <a href="#"></a>
-    
       </div>
 
       <center>
 
            <div class="photo-container">
-        
              <img src="../php/imagem.png" 
               height="250"
               width="280">
 
           </div>
 </center>
-  
   </nav>
   </div> 
   </div>
   </div>
   </div>
   <center>
+
+  
+  <?php
+      $servername = "localhost";
+      $username = "lumedigital";
+      $password = "biblio";
+      $dbname = "livros";
+
+      $conn= new mysqli("localhost", "lumedigital", "biblio", "livros");
+
+
+      if ($conn->connect_error) {
+          die("Conexão falhou: " . $conn->connect_error);
+      }
+
+      $termo = $_POST['pesquisa'];
+
+      $sql = "SELECT * FROM planilha1 WHERE C LIKE'%" . $termo . "%'";
+
+      $resultado = $conn->query($sql);
+      $result = $conn->query($sql);
+
+if ($result === false) {
+    echo "Erro na consulta: " . $conn->error;
+} elseif ($result->num_rows > 0) { 
+    while ($row = $result->fetch_assoc()) {
+        echo "LIVRO EXISTENTE: " . $row["C"] . "<br>";
+    }
+} else {
+
+    echo "Nenhum resultado encontrado.";
+}
+
+
+
+ ?>
---
+++ trabalho/sobre.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="menu.css">
+  <link rel="stylesheet" href="barra.css">
+  
+  
+  <style>
+     .container {
+            background: #ab97d1;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            width: 400px;
+            text-align: center;}
+     
+        h2 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+        }
+        label {
+            margin-bottom: 5px;
+            font-weight: bold;
+            text-align: left;
+        }
+        input {
+            margin-bottom: 15px;
+            padding: 10px;
+            border: 1px solid #414c87;
+            border-radius: 4px;
+        }
+        button {
+            background-color: #a879c7;
+            color: white;
+            border: none;
+            padding: 10px;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+        button:hover {
+            background-color: #ca45c4;
+        }
+  </style>
+  
+</head>
+<body>
+
+  <nav class="navbar">
+    <div class="logo">Lume Digital</div>
+    
+    <center>
+
+      <div class="logo">Sobre o Centro de Línguas de Brazlândia</div> 
+    
+</center>
+
+   <li> <h2>BIBLIOTECA</h2></li>
+       
+
+
+    </ul>
+  </nav>
+
+
+</div>
+</li>
+
+
+    <div class="sidebar_menu">
+        <div class="sidebar_titulo">
+             <a href="#">Painel de Controle</a>
+        </div>
+  
+
+
+    <nav class="menu">
+        <ul>
+            <li>
+                <i class="fas fa-key"></i>
+                <a href="biblioteca.php">Bibliotecário</a>
+            </li>
+            <br>
+            <li>
+                <i class="fa-solid fa-user"></i>
+                <a href="leitor.php">Aluno</a>
+            </li>
+            <br>
+            <li>
+                <i class="fa-solid fa-book"></i>
+                <a href="#">Meus livros</a>
+            </li>
+            <br>
+            <li>
+                <i class="fas fa-stream"></i>
+                <a href="#">Mensagem</a>
+            </li>
+            <br>
+            <li>
+                <i class="fas fa-question-circle"></i>
+                <a href="#">Suporte</a>
+            </li><br>
+            <li>
+                <i class="fas fa-home"></i>
+                <a href="cilbinicial.php">Início</a>
+            </li>
+        </ul>
+    </nav>
+    </div> 
+    </div>
+    </div>
+    </div>
+</nav>
+
+    <div class="menu">
+      <input type="checkbox" id="check">
+      <div class="menu-icon">
+        <li>
+          <label for="check">
+              <i class="fas fa-bars"></i>
+          </label>
+     
+</nav>
+
+    <div class="menu">
+      <input type="checkbox" id="check">
+      <div class="menu-icon">
+        <li>
+          <label for="check">
+              <i class="fas fa-bars"></i>
+          </label>
+     
+</div>
+</li>
+
+
+  <div class="sidebar_menup">
+      <div class="sidebar_titulop">
+           <a href="#"></a>
+    
+      </div>
+
+      <center>
+
+           <div class="photo-container">
+        
+             <img src="../php/imagem.png" 
+              height="250"
+              width="280">
+
+          </div>
+</center>
+  
+  </nav>
+  </div> 
+  </div>
+  </div>
+  </div>
+  <center>
+
+<p><h4> O Centro Interescolar de Línguas  de Brazlândia -  CILB, </h4> localizado no Setor Tradicional  é uma Escola Pública de idiomas  que atende  alunos do 6° Ano ao Ensino Médio,<br>  Educação de Jovens e Adultos e Comunidade em geral.<br>
+O CILB iniciou seus trabalhos sob a direção da Professora Sandra,<br> depois pela Professora Maria Danizete e, atualmente, pela gestão das Professoras<br><h4> Cássia Lima (Diretora) e Izabel Guimarães (Vice-Diretora).</h4><br> 
+Atualmente, o CILB oferece os idiomas de Inglês,  Espanhol e Francês,<br> funcionando nos turnos matutino e vespertino.<br>
+  A escola possui uma biblioteca que oferece  acervo diversificado <br> nas línguas portuguesa, inglesa, 
+ espanhola, francesa e espanhola.<br>
+  A mesma é extensão da sala de aula, onde oferece recursos didáticos-pedagógicos<br> para os alunos aprimorarem seus conhecimentos no idioma estudado.<br>
+   Inicialmente, a biblioteca recebeu o nome de <h4>Centro de Pesquisas.</h4><br>  E em homenagem póstumas à Professora Telma Cristiane que foi uma assídua leitora, amante e incentivadora 
+ da leitura,<br> o CP, recebeu o nome dela:<h4> Biblioteca Professora Telma Cristiane.</h4></p><br>
+
+<div class="container">
+        <h2>SUPORTE</h2>
+        <form action="" method="post">
+            <label for="email">Digite seu E-mail:</label><br>
+            <input type="email" id="email" name="email" required>
+          
+           <label for= "other" > Qual é a sua dúvida? </label><br>
+           <textarea name= "other" rows="4" cols="40"> </textarea>  <br /> 
+          
+            <form method="post" action="">
+            <button type="submit">Enviar</button><br>
+    </form>
+
+
+
+</center>
