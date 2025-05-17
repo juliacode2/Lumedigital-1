40c662725bb9a5c905ca9b6d57aeb2383549983e
juliacode2 <2126juba@gmail.com>
2025-05-17 13:15:29 -0300

    LumeDigital
    projeto de conclusão de curso

--- trabalho/leitor.php
+++ trabalho/leitor.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../css/index.css">
     <title>Página de Login</title>
     <style>
 
@@ -57,7 +59,9 @@
         }
     </style>
 </head>
+
 <body>
+
 
 
     <div class="container">
@@ -76,7 +80,7 @@
 
         <form method="post" action="editar_excluir_f.php">
         <button type="submit">Enditar/Excluir</button>
-</form>
+</form><br>
 
         <a href="cad1.php">
         <h5>Fazer Cadastro</h5>
